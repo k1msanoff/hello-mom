07305033f41461828ecd283bd32f134548e302b0
Mukeem <115736183+k1msanoff@users.noreply.github.com>
2025-01-05 11:07:52 +0500

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(1);
+
+
+const маруф = document.querySelector('button');
+const барор = document.querySelector('.output');
+
+маруф.addEventListener('click', function() {
+    барор.innerHTML = "Mom, I love you 🤍";
+})
